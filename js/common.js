c3b5c912e33a395a0164001f71f00d52833ff2cc
Zhemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2018-05-15 00:03:21 +0300

    Завершённая страничка Бьюти

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 $(function() {
 
-	// Custom JS
+	// Accordion menu
+	
+	$('.nav__list-second').hide();
 
+	$('.nav__list*>li').click(function() {
+		var findSubmenu = $(this).find('.nav__list-second');
+
+		if (findSubmenu.is(':visible')) {
+			findSubmenu.slideUp(600);
+		} else {
+			$('.nav__list-second').slideUp(600);
+			findSubmenu.slideDown(600);
+		}
+
+	});
 });
